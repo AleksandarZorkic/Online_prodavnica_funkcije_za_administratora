834969129a82eed242175c46c7438fe2ea1fec00
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-05-08 17:06:53 +0200

    Dodao sam i formu za dodavanje novog prozivoda koja ispunajva zahteve zadatka.

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ class Artikal {
     }
 }
 
+let artikli = []
 function createProductRows(artikli) {
     let table = document.querySelector("#product-body")
+    table.innerHTML = ""
 
 
     for(let i = 0; i < artikli.length; i++) {
@@ -47,13 +49,36 @@ function displayProductDetails(artikal) {
   }
 
 function initializeProducts() {
-    let artikli = [
+    artikli = [
         new Artikal("Monitor", 165, "Monitor XVision 27Q je elegantan 27-inčni ekran sa QHD rezolucijom (2560x1440), idealan za svakodnevni rad, gejming i multimediju."),
         new Artikal("TV", 650, "Samsung Crystal UHD 55CU8002 je moderan 55-inčni pametni televizor sa 4K rezolucijom koji nudi izuzetno oštru sliku i bogat spektar boja zahvaljujući Crystal Processor 4K tehnologiji."),
         new Artikal("Mis", 20, "LogiX M720 je ergonomski bežični miš dizajniran za udobnost tokom celodnevnog rada.")
     ]
 
     createProductRows(artikli)
+    handleFormSubmission(artikli)
 }
 
 document.addEventListener('DOMContentLoaded', initializeProducts)
+
+function handleFormSubmission(artikli) {
+    let submitBtn = document.querySelector("#submitBtn")
+    submitBtn.addEventListener('click', function() {
+        const forma = document.querySelector('#forma')
+        const formData = new FormData(forma)
+
+        const naziv = formData.get("naziv")
+        const cena = formData.get("cena")
+        const opis = formData.get("opis")
+
+        for (let i = 0; artikli.length; i++) {
+            if (naziv === artikli[i].name)
+                return
+        }
+
+        const noviArtikal = new Artikal(naziv, cena, opis)
+        artikli.push(noviArtikal)
+
+        createProductRows(artikli)
+    })
+}
